6285a18df9dad8d7e20d77c980d35bf88eaee60b
maning <154292322@qq.com>
2019-12-02 11:09:09 +0800

    去除requestWindowFeature(Window.FEATURE_NO_TITLE);

--- imagebrowserlibrary/src/main/java/com/maning/imagebrowserlibrary/MNImageBrowserActivity.java
+++ imagebrowserlibrary/src/main/java/com/maning/imagebrowserlibrary/MNImageBrowserActivity.java
@@ -84,7 +84,6 @@ public class MNImageBrowserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_mnimage_browser);
         sActivityRef = new WeakReference<>(this);
         context = this;
