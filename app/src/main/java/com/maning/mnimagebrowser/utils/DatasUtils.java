62f89b1ab57df7f7622ae5665bb4d04a57c2618d
maning <154292322@qq.com>
2020-11-23 15:50:21 +0800

    添加一个坏的图片地址

--- app/src/main/java/com/maning/mnimagebrowser/utils/DatasUtils.java
+++ app/src/main/java/com/maning/mnimagebrowser/utils/DatasUtils.java
@@ -9,6 +9,7 @@ public class DatasUtils {
         sourceImageList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1523527461022&di=cf56170de9cd14bd24299af8597ee88d&imgtype=0&src=http%3A%2F%2Fpic13.nipic.com%2F20110414%2F7056815_094945110000_2.jpg");
 
         sourceImageList.add("https://i.loli.net/2020/07/03/4eYAaqHP8TbzFlO.png");
+        sourceImageList.add("https://i.loli.net/2020/07/03/4eYAaqHP8TbzFlO.pn");
         sourceImageList.add("https://i.loli.net/2020/07/03/vf59KgBpw2SAd8Z.jpg");
         sourceImageList.add("https://i.loli.net/2020/07/03/R2gmx8M6Ipq9Tet.jpg");
         sourceImageList.add("https://i.loli.net/2020/07/03/rJoGVeEAuaR2vUc.jpg");
